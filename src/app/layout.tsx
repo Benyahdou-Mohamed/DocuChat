f1cb558e22f2a7d76f90504e955294381acb3807
benyahdou-mohamed <benyahdou.moh29@hotmail.com>
2024-04-20 19:32:01 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import Navbar from "@/components/Navbar";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "DocuChat",
+  description: "Turn PDFs into conversations.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="light">
+      <body className={cn("min-h-screen font-sans antialiased grainy",inter.className)}>
+        <Navbar/>
+        {children}</body>
+    </html>
+  );
+}
